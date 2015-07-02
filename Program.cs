fc39be29b588b43b993a1fd8170d5e1aca15d6db
cdmarasinghe <cdmarasingheit@gmail.com>
2015-07-02 14:56:43 +0530

    edited

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Q1
+{
+    class Program
+    {
+        public void multiples()
+        {
+            for (int i = 1; i <= 100; i++)
+            {
+                if (i % 3 == 0 || i % 5 == 0)
+                {
+                    if (i % 3 == 0 && i % 5 == 0)
+                        Console.WriteLine("SupportTraining");
+                    else
+                    {
+                        if (i % 3 == 0)
+                            Console.WriteLine("Support");
+                        if (i % 5 == 0)
+                            Console.WriteLine("Training");
+                    }
+                }
+                else
+                    Console.WriteLine(i);
+            }
+        }
+        static void Main(string[] args)
+        {
+            Program obj = new Program();
+            obj.multiples();
+        }
+    }
+}
